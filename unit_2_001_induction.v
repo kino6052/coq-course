2ea6bc368eda92f201e0ff24ccb85644ce47ecb4
Kirill <kirill.novik.csci@gmail.com>
2022-03-25 12:52:17 -0700

    Fix the Auxiliary Proof

--- unit_2_001_induction.v
+++ unit_2_001_induction.v
@@ -44,7 +44,70 @@ Proof.
   }
 Qed.
 
-Theorem plus_assoc : ∀ n m p : nat,
+Theorem plus_assoc : forall n m p : nat,
   n + (m + p) = (n + m) + p.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros. 
+  induction n.
+  - simpl. reflexivity.
+  - simpl. rewrite <- IHn. reflexivity.
+Qed.
+
+Lemma minus_Sn_n : forall n, S n - n = 1.
+Proof.
+  intros.
+  induction n.
+  - reflexivity.
+  - simpl in IHn. simpl. rewrite -> IHn. reflexivity.
+Qed.
+
+Fixpoint double (n:nat) :=
+  match n with
+  | O => O
+  | S n' => S (S (double n'))
+  end.
+
+Lemma double_plus : forall n, double n = n + n.
+Proof.
+  intros.
+  induction n.
+  - reflexivity.
+  - simpl. rewrite -> IHn. rewrite <- plus_n_Sm. reflexivity.
+Qed.
+
+Lemma double_plus' : forall n, double n = n + n.
+Proof.
+  (* Auxiliary proof *)
+  assert (H1: forall m k, S(m + k) = m + (S k)).
+  { intros. induction m.
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHm. reflexivity.  
+  }
+  intros.
+  induction n.
+  - reflexivity.
+  - simpl. rewrite -> IHn. simpl in H1. rewrite <- H1. reflexivity.
+Qed.
+
+Theorem evenb_S : forall n : nat,
+  evenb (S n) = negb (evenb n).
+Proof.
+  intros.
+  induction n.
+  - simpl. reflexivity.
+  - rewrite -> IHn. simpl. rewrite -> negb_involutive. reflexivity.
+Qed.
+
+Theorem mult_0_plus' : forall n m : nat,
+  (0 + n) * m = n * m.
+Proof.
+  intros n m.
+  assert (H: 0 + n = n). { reflexivity. }
+  rewrite -> H.
+  reflexivity. 
+Qed.
+
+
+
+
+ 
