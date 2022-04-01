962b46356c633fd90ea60c47f4af451b1222c4cf
Kirill <kirill.novik.csci@gmail.com>
2022-04-01 13:47:23 -0700

    Start Unit 5 - Tactics

---
+++ unit_5_tactics.v
@@ -0,0 +1,24 @@
+Add LoadPath "$COQ_PROOFS" as Path.
+Load unit_3_001_structured_data.
+
+Theorem silly1 : forall (n m o p : nat),
+     n = m ->
+     [n;o] = [n;p] ->
+     [n;o] = [m;p].
+Proof.
+  intros n m o p eq1 eq2.
+  rewrite <- eq1.
+  rewrite eq2.
+  reflexivity.
+Qed.
+
+Theorem silly1' : forall (n m o p : nat),
+     n = m ->
+     [n;o] = [n;p] ->
+     [n;o] = [m;p].
+Proof.
+  intros n m o p eq1 eq2.
+  rewrite <- eq1.
+  apply eq2.
+Qed.
+
