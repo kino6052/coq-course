af0981918404d98554bb5c5c9c5ce397e82bfe33
Kirill <kirill.novik.csci@gmail.com>
2022-04-03 10:16:31 -0700

    Add Infinite Number Proof with Filter

--- numbers_proof.v
+++ numbers_proof.v
@@ -36,15 +36,29 @@ Proof.
 Qed.
 
 
-Theorem test: forall (m: nat), exists (n: nat), grb n m = true.
+Theorem inf_nat: forall (m: nat), exists (n: nat), grb n m = true.
 Proof.
   intros m.
   induction m.
   - exists 1. simpl. reflexivity.
-  - exists (m + 2). simpl. induction m.
-    + simpl. reflexivity.
-    + simpl. rewrite IHm0. 
-      * reflexivity. 
-      * exists (S m). apply t'.
+  - exists (S (S m)). simpl. apply t' with (n:=S m). 
 Qed.
 
+Lemma wrong: forall x: nat, 2 * x = 1 -> False.
+Proof.
+  intros x. induction x.
+  - intros. inversion H.
+  - intros. apply IHx. destruct x. inversion H. inversion H. 
+Qed.
+
+Theorem inf_nat_filter': forall (m: nat), exists (n: nat), grb n m = true /\ ~ exists (k: nat), 2 * k = n.
+Proof.
+  intros m.
+  induction m.
+  - exists 1. unfold not. split.
+    + simpl. reflexivity.
+    + intros. destruct H. apply wrong with (x:=x). apply H.
+  - exists (S (S m)). unfold not. split.
+    + apply t' with (n:=S m).
+    (* Stuck *)
+    + intros. destruct H. destruct IHm. unfold not in H0. destruct H0. apply H1.
